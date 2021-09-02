240bf7c6c6f9b75f3a16344ec127b9ac8bdc0493
CemachMD <cesar@md-online.com.br>
2021-09-02 19:50:20 -0300

    Iniciando Test RpcClient

---
+++ DriverAmqp.Sources.Tests/RpcClientTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xunit;
+
+namespace DriverAmqp.Sources.Tests
+{
+    public class RpcClientTest
+    {
+        [Fact]
+        public void RpcClientRun()
+        {
+            //Arrange
+            var amqp = WrapperConnection.GetInstance();
+            var ch = amqp.CreateChannel();
+            var rpcServer = new RpcServer(ch);
+            rpcServer.Init();
+            rpcServer.Start();
+            var result = rpcServer.IsRunning();
+
+            //Act
+
+            //Assert
+            Assert.True(result);
+        }
+    }
+}
